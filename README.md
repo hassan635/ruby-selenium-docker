63c7563a24ba9c3f8285a1ad08dc555e58d97a6c
hassan635 <hassan_030@yahoo.com>
2020-03-09 16:13:13 +1100

    Initial commit for rspec backed selenium script

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # ruby-selenium-docker
+
+Just an sample script to show Selenium webdriver usage through Ruby and Rspec
+
+As always, Docker is the only pre-req!
+
+Run the following docker run command:
+
+```Bash
+docker run --rm -it --name ruby-selenium -v $PWD:/home/app -w /home/app nbulai/ruby-chromedriver /bin/bash
+```
+
+This will provision and return back a root shell inside the container. Follow these steps to run the sample script(s)
+
+- Install the required gems by running the following commands:
+```
+gem install selenium-webdriver
+```
+```
+gem install rspec
+```
+- Clone this repo and change into the rspec-scripts directory
+- Run this command:
+```
+rspec selenium-ui-test.rb
+```
+- Once the test passes, there would be a snapshot for the successful execution as well. It's a snap of my LinkedIn profile. Yeah I'm a shameless self-promotor. Get over it :)
---
+++ rspec-scripts/selenium-ui-test.rb
@@ -0,0 +1,32 @@
+require 'selenium-webdriver'
+require 'rspec'
+require 'rspec/expectations'
+extend RSpec::Matchers
+
+describe "Search for Hasan's LinkedIn profile" do
+    before(:each) do
+        options = Selenium::WebDriver::Chrome::Options.new()
+        options = Selenium::WebDriver::Chrome::Options.new
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        @driver = Selenium::WebDriver.for(:chrome, options: options)
+        @base_url = "https://www.google.com/"
+    end
+
+    context "Visit profile" do
+        it "verifies the expected title" do
+            @driver.get(@base_url)
+            wait = Selenium::WebDriver::Wait.new(:timeout => 15)
+            search_box = wait.until { @driver.find_element(:name, 'q') }
+            search_box.send_keys "hasan ali tariq test automation engineer linkedIn"
+            search_box.send_keys :enter
+            my_linkedIn_profile_link = wait.until { @driver.find_element(:css, 'a[href$=\'hassanali635\']') }
+            my_linkedIn_profile_link.click
+            my_linkedIn_profile_title = wait.until { @driver.find_element(:xpath, '//h1[contains(text(),\'Hasan Ali Tariq\')]') }
+            title_text = my_linkedIn_profile_title.text
+            expect(title_text).to   eq('Hasan Ali Tariq')
+            @driver.save_screenshot('/home/app/rspec-scripts/profile.png')
+            @driver.quit
+        end
+    end
+end
